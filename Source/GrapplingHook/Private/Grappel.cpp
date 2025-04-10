cd380dec1c6ec5b6cf7610fa69387c6a017a690e
puuske <pontus.andersson93@hotmail.com>
2025-04-10 20:35:59 +0200

    GrappelHook module + Grappel class

---
+++ Source/GrapplingHook/Private/Grappel.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grappel.h"
+
+// Sets default values
+AGrappel::AGrappel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGrappel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGrappel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
