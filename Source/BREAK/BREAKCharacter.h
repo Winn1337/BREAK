81db106b571565e10c5e1304f88266b9ca7c610c
puuske <pontus.andersson93@hotmail.com>
2025-04-16 13:36:52 +0200

    Seperated grapple logic from Character

--- Source/BREAK/BREAKCharacter.h
+++ Source/BREAK/BREAKCharacter.h
@@ -5,8 +5,6 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
-#include "InputActionValue.h"
-#include "Grappel.h"
 #include "BREAKCharacter.generated.h"
 
 class UInputComponent;
@@ -15,10 +13,11 @@ class UCameraComponent;
 class UInputAction;
 class UInputMappingContext;
 struct FInputActionValue;
+class UBREAKWeaponComponent;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
 
-UCLASS(config = Game)
+UCLASS(config = Game, Blueprintable, BlueprintType)
 class ABREAKCharacter : public ACharacter
 {
 	GENERATED_BODY()
@@ -45,49 +44,31 @@ class ABREAKCharacter : public ACharacter
 
 	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* LookAction;
-
-	/** Grapple Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* GrappleAction;
-
-	/** Grapple actor class to spawn */
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Grappling, meta = (AllowPrivateAccess = "true"))
-	TSubclassOf<AGrappel> GrappleClass;
+	class UInputAction* LookAction;
 
 public:
 	ABREAKCharacter();
 
 protected:
+
 	/** Called for movement input */
 	void Move(const FInputActionValue& Value);
 
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
 
-	/** Called to fire the grappling hook */
-	void FireGrapple();
-
-	/* Called to release the grapple */
-	void ReleaseGrapple();
-
-	/** Per-frame logic */
-	virtual void Tick(float DeltaTime) override;
-
 protected:
 	// APawn interface
 	virtual void NotifyControllerChanged() override;
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
 
 public:
 	/** Returns Mesh1P subobject **/
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
-
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
-private:
-	/** Currently active grapple instance */
-	UPROPERTY()
-	AGrappel* ActiveGrapple;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = RocketLauncher)
+	UBREAKWeaponComponent* RocketLauncherComponent;
 };
