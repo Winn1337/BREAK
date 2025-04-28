0f1df378a5101d32595b03ae971477e60e8a4bf5
Victor Gustafsson <112407625+ViggeGee@users.noreply.github.com>
2025-04-28 13:03:05 +0200

    new grapple sound and timer pauses on win

--- Source/GrapplingHook/Public/Grappel.h
+++ Source/GrapplingHook/Public/Grappel.h
@@ -26,8 +26,10 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+	virtual ~AGrappel();
 
 private:
+
 	UPROPERTY(VisibleAnywhere)
 	USceneComponent* Root;
 
